819d79e1f175ab9c45255b9317a536cc71292d4c
Suphanat Jarukulgowit <suphanat.jth@gmail.com>
2022-11-27 00:51:21 +0700

    delete comment

--- test/keyword_test.go
+++ test/keyword_test.go
@@ -26,27 +26,7 @@ func (m *mockGormDb) Rows() (*sql.Rows, error) {
 	return r, nil
 }
 
-func TestGetKeywordList(t *testing.T) {
-	config, err := configuration.LoadConfigFile("../config/config.local.json")
-	if err != nil {
-		assert.Nil(t, err)
-	} else {
-		di.Init(config)
-		// h, _ := handler.NewKeywordsHandler(config, config.GoogleSearchApiKey)
-		// keywordervice, err := di.GetKeywordService()
-		// if err != nil {
-
-		// }
-		// mockGormDb := &mockGormDb{}
-		// response, err := keywordervice.GetKeywordList(mockGormDb, "")
-		// if err != nil {
-		// 	assert.Nil(t, err)
-		// }
-		// fmt.Println(response)
-	}
-}
-
-func TestGetKeywordListWithLib(t *testing.T) {
+func TestGetKeywordListWith(t *testing.T) {
 	config, err := configuration.LoadConfigFile("../config/config.local.json")
 	if err != nil {
 		assert.Nil(t, err)
@@ -58,8 +38,6 @@ func TestGetKeywordListWithLib(t *testing.T) {
 		}
 		db, mock, sqlDb := NewDatabase()
 		defer sqlDb.Close()
-		// a SELECT VERSION() query will be run when gorm opens the database
-		// so we need to expect that here
 		columns := []string{"id, keyword, search_id, ad_words, links, html_link, search_results, time_taken, cache, created_date, user_id, status, err_msg"}
 		userIDMock := "6623CCE882D645338D1F5548F35B32FE"
 		mock.ExpectQuery(`select id, keyword, ad_words, links, html_link, raw_html, search_results, time_taken, created_date, cache from google_search_api_detail_dbs where user_id = ? ORDER BY created_date asc`).WithArgs(userIDMock).WillReturnRows(mock.NewRows(columns))
